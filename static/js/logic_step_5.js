2935c9b88df071e291e2d174b9dee179d9f86b96
kaalrok <92065902+kaalrok@users.noreply.github.com>
2022-03-13 13:32:43 +0100

    updating

--- static/js/logic_step_5.js
+++ static/js/logic_step_5.js
@@ -63,8 +63,9 @@ let overlays = {
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
 L.control.layers(baseMaps, overlays, {
-    collapsed: false}
-    ).addTo(map);
+    collapsed: false
+}
+).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 // dark.addTo(map);
@@ -202,3 +203,35 @@ d3.json(earthquakeData).then(function (data) {
     // Then we add the earthquake layer to our map
     earthquakes.addTo(map);
 });
+
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+    ];
+
+    // Looping through our intervals to generate a label with a colored square for each interval.
+   for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+ }
+  return div;
+};
+
+legend.addTo(map);
+
